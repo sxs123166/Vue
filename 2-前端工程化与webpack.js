97b0110a44034ef364250dfb768fc7311bacc1f0
xuesongshang <shangxuesong@duxiaoman.com>
2023-03-20 16:10:13 +0800

    第一次提交

---
+++ 2-前端工程化与webpack.js
@@ -0,0 +1,29 @@
+// 目录
+// 前端工程化
+// webpack的基本使用
+// webpack中的插件
+// webpack重的Loader
+// 打包发布
+// Source Map
+
+// 1. 什么是前端工程化
+
+// 实际的前端开发：
+// 模块化（js的模块化、css的模块化、资源的模块化）
+// 组件化（复用现有的UI结构、样式、行为）
+// 规范化（目录结构的划分、编码规范化、接口规范化、文档规范化、Git分支管理）
+// 自动化（自动化构建、自动部署、自动测试）
+
+// 前端工程化指的是：在企业级的开发项目中，把前端开发所需的工具、技术、流程、经验等进行规范化、标准化
+
+// 企业中的Vue和React项目，都是基于工程化的方式进行开发的
+
+// 早期的前端工程化解决方案：
+// grunt(https://www.gruntjs.net/)
+// gulp(https://www.gulpjs.com.cn/)
+
+// 目前主流的前端工程化解决方案：
+// webpack(https://www.webpackjs.com/)
+// parcel(https://zh.parceljs.org/)
+
+
