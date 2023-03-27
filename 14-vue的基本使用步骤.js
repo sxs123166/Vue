fa0ff4f3d7125e729a5c6bd5a2d76ff679340a23
Cedar <1752440588@qq.com>
2023-03-27 22:48:59 +0800

    .

---
+++ 14-vue的基本使用步骤.js
@@ -0,0 +1,22 @@
+
+// 1. 基本使用步骤
+// 1） 导入vue.js的script脚本文件
+// 2）在页面中声明一个将要被vue所控制的DOM区域
+// 3）创建vm实例对象（vue实例对象）
+
+/* <body> */
+  //在页面中声明一个将要被vue所控制的DO区域
+  //<div id="app">{{ username }}<div/>
+  
+//   1. 导入vue.js的script脚本文件
+//<script src="./lib/ue-2.6.12.js"></script>
+//   3.创建vm实例对象
+    //  const vm = new Vue({
+    //     // 3.1 指定当前vm实例要控制页面的哪个区域
+    //     el: '#app',
+    //     // 3.2 指定Model数据源
+    //     data: {
+    //         username: 'zs'
+    //     }
+    //  })
+    /* <body/> */
