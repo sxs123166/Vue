31f84f6ab1e23b1be33f8d4f7f32f380542715ae
Cedar <1752440588@qq.com>
2023-04-08 17:10:03 +0800

    组件

---
+++ 36-vue项目的运行流程.js
@@ -0,0 +1,8 @@
+
+// vue项目的运行流程
+// 在工程化的项目中，vue要做的事情很单纯：通过main.js把app.vue渲染到index.html的指定区域中
+
+// 其中：
+// 1）App.vue用来编写待渲染的摸板结构
+// 2）index.html中需要预留一个el区域
+// 3）main.js把App.vue渲染到了index.html所预留的区域中
