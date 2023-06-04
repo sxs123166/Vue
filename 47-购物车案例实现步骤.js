8daa4423010176fd1daf0f61e73090a5051c093b
Cedar <1752440588@qq.com>
2023-06-04 21:57:07 +0800

    购物车案例开端

---
+++ 47-购物车案例实现步骤.js
@@ -0,0 +1,12 @@
+// 1.初始化项目结构
+// 2.封装MyHeader组件
+// 3.基于axios请求商品列表数据
+// 4.封装MyFooter组件
+// 5.封装MyGoods组件
+// 6.封装MyCounter组件
+
+// 基于axios请求列表数据
+// 1.安装并在app.vue中导入axios
+// 2.在methods方法中，定义initCartList函数请求列表数据
+// 3.在created生命周期函数中，调用步骤2封装的initCartList函数
+
